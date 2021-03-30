bbe85f2c7251a33da251c6ebb682c83e6abca124
miguel dias <miguel.dias.1212@gmail.com>
2021-03-30 01:11:03 +0100

    deploying dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,11 +5,16 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 engine=create_engine(os.environ['DB_URI'])
+
 df = pd.read_sql(
     "select * from submission_db where length(slack_id) = 11",
     engine
 )
-ax = df.groupby('learning_unit').slack_id.count().plot.bar(
+df_plot = df.groupby('learning_unit').slack_id.count()
+
+st.dataframe(df_plot, width=1000, height=1000)
+
+ax = df_plot.plot.bar(
     rot=0,
     title='How many students have completed each Learning Unit'
 )
@@ -17,4 +22,3 @@ st.pyplot(
     ax.get_figure()
 )
 
-# st.dataframe(df, width=700, height=1000)
