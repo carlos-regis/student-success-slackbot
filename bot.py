37055a25258e8147c13ff6b072b81fbd221fde41
Mariana Fernandes <marianasf13@hotmail.com>
2022-04-13 21:42:23 +0100

    Add requirements and update bot script

--- bot.py
+++ bot.py
@@ -1,21 +1,29 @@
 import os
 from typing import Set
-
+import time
+from cv2 import log
 import pandas as pd
-from sqlalchemy import create_engine
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from get_submissions import get_submitted_slack_ids
+import logging
 
-# I used the following 2 lines of code because I was getting an SSL error. If 
-# you don't get that error, feel free to comment them.
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+logging.basicConfig(level=logging.INFO)
+
+INSTRUCTORS_CHANNEL_ID = "C036U6ZFEKC"
+URL = 'https://prep-course-portal.ldsacademy.org/submissions/'
+SLU_ID = os.environ["SLU_ID"]
+
+# Number of maximum attemps to post a message in a channel
+MAX_ATTEMPTS = 5
+
+client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
 def get_instructors_ids(client: WebClient) -> Set[str]:
     '''
     Gets the slack_ids of the instructors to save them from spam.
     '''
-    response = client.conversations_members(channel="C01RBSVJ1D5")
+    response = client.conversations_members(channel=INSTRUCTORS_CHANNEL_ID)
     instructors_ids = response["members"]
 
     return set(instructors_ids)
@@ -32,60 +40,58 @@ def get_all_slack_ids(client: WebClient) -> Set[str]:
     
     return set(user_ids)
 
+def reminder_message(slu_id):
+    slu_id_str = SLU_ID
+    if len(slu_id_str) == 1:
+        slu_id_str = '0' + slu_id_str
 
-def get_submitted_slack_ids() -> Set[str]:
+    text = f'''
+    You have not submitted SLU{slu_id_str} yet! 
+    Please do, the deadline is today!
+    If you need help, go to the week-{slu_id_str} channel and ask!
     '''
-    Gets the slack_ids of all the human users in the workspace 
-    that have submitted the lu.
-    (the ones that have lenght 11 - this is specific to this workspace).
-    '''
-    engine = create_engine(os.environ['DB_URI'])
-    df = pd.read_sql(
-        "select * from submission_db where length(slack_id) = 11",
-        engine
-    )
-    submitted_slack_ids = set(
-        df.loc[
-            (df.learning_unit == int(os.environ["SLU"])) 
-            & 
-            (df.slack_id.str.len() == 11), 
-            'slack_id'
-        ]
-    )
+    return text
 
-    return submitted_slack_ids
+def send_message(slack_id, slu_id):
+    """
+    Send a Slack message to student to remind them to
+    submit the SLU
+    """
+    while n < MAX_ATTEMPTS:
+        n = 0
+        try:
+            result = client.chat_postMessage(
+                channel='C03BB9M2UNR', #slack_id
+                text=reminder_message(slu_id)
+            )
+            if result['ok']:
+                n_messages_sent += 1
+                logging.info(slack_id)
+            time.sleep(1)
+            break
+            
+        except SlackApiError as e:
+            time.sleep(1)
+            logging.warning(f"Error: {e}")
+            n += 1
+    
+    if n == MAX_ATTEMPTS:
+        logging.warning("Number of max unsuccessful attempts reached. Message was not sent.")
+    
+if __name__ == '__main__':
+    all_slack_ids = get_all_slack_ids(client)
+    instructors_ids = get_instructors_ids(client)
+    submitted_slack_ids = get_submitted_slack_ids(SLU_ID, URL)
+    not_submitted_slack_ids = all_slack_ids - submitted_slack_ids - instructors_ids
 
+    logging.info(f'Total number of Slack IDs: {len(all_slack_ids)}')
+    logging.info(f'Number of Instructor IDs: {len(instructors_ids)}')
+    logging.info(f'Number of IDs with submissions: {len(submitted_slack_ids)}')
+    logging.info(f'Number of not submitted IDs: {len(not_submitted_slack_ids)}')
 
-client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
-
-all_slack_ids = get_all_slack_ids(client)
-instructors_ids = get_instructors_ids(client)
-submitted_slack_ids = get_submitted_slack_ids()
-not_submitted_slack_ids = all_slack_ids - submitted_slack_ids - instructors_ids
-
-print(
-    len(all_slack_ids),
-    len(instructors_ids),
-    len(submitted_slack_ids),
-    len(not_submitted_slack_ids),
-)
-
-text = f'''
-You have not submitted SLU{os.environ["SLU"]} yet! 
-Please do, the deadline is today!
-If you need help, go to the week-{os.environ["SLU"]} channel and ask!
-'''
-
-n_messages_sent = 0
-for slack_id in not_submitted_slack_ids:
-    try:
-        result = client.chat_postMessage(
-            channel=slack_id,
-            text=text
-        )
-        if result['ok']:
-            n_messages_sent += 1
-    except SlackApiError as e:
-        print(f"Error: {e}")
-
-print(n_messages_sent)
+    logging.info(not_submitted_slack_ids)
+    n_messages_sent = 0
+    for slack_id in not_submitted_slack_ids:
+        send_message(slack_id=slack_id, slu_id=SLU_ID)
+   
+    logging.info(f'{n_messages_sent} students have received a reminder message on SLU {SLU_ID}.')
