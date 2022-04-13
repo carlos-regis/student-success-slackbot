3273dc36898daf02d992c013cdf2cf8208324c54
Mariana Fernandes <marianasf13@hotmail.com>
2022-04-13 22:55:08 +0100

    Fix slu_id to be int

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 from typing import Set
 import time
-from cv2 import log
 import pandas as pd
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -12,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 
 INSTRUCTORS_CHANNEL_ID = "C036U6ZFEKC"
 URL = "https://prep-course-portal.ldsacademy.org/submissions/"
-SLU_ID = os.environ["SLU_ID"]
+SLU_ID = int(os.environ["SLU_ID"])
 
 # Number of maximum attemps to post a message in a channel
 MAX_ATTEMPTS = 5
@@ -20,11 +19,11 @@ MAX_ATTEMPTS = 5
 client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
 
-def get_instructors_ids(client: WebClient) -> Set[str]:
+def get_instructors_ids(client: WebClient, channel_id: str) -> Set[str]:
     """
     Gets the slack_ids of the instructors to save them from spam.
     """
-    response = client.conversations_members(channel=INSTRUCTORS_CHANNEL_ID)
+    response = client.conversations_members(channel=channel_id)
     instructors_ids = response["members"]
 
     return set(instructors_ids)
@@ -43,8 +42,8 @@ def get_all_slack_ids(client: WebClient) -> Set[str]:
     return set(user_ids)
 
 
-def reminder_message(slu_id):
-    slu_id_str = SLU_ID
+def reminder_message(slu_id: int) -> str:
+    slu_id_str = str(SLU_ID)
     if len(slu_id_str) == 1:
         slu_id_str = "0" + slu_id_str
 
@@ -56,37 +55,36 @@ def reminder_message(slu_id):
     return text
 
 
-def send_message(slack_id, slu_id):
+def send_message(slack_id: str, slu_id: int):
     """
     Send a Slack message to student to remind them to
     submit the SLU
     """
+    n = 0
     while n < MAX_ATTEMPTS:
-        n = 0
         try:
             result = client.chat_postMessage(
-                channel="C03BB9M2UNR", text=reminder_message(slu_id)  # slack_id
+                channel="C03BB9M2UNR", text=reminder_message(slu_id)
             )
             if result["ok"]:
-                n_messages_sent += 1
                 logging.info(slack_id)
-            time.sleep(1)
+                time.sleep(0.1)
             break
 
         except SlackApiError as e:
             time.sleep(1)
-            logging.warning(f"Error: {e}")
+            logging.warning(f"Error on Slack API on try #{n} for {slack_id}: {e}")
             n += 1
 
     if n == MAX_ATTEMPTS:
         logging.warning(
-            "Number of max unsuccessful attempts reached. Message was not sent."
+            "Number of max unsuccessful attempts reached. Message was not sent to {slack_id}."
         )
 
 
 if __name__ == "__main__":
     all_slack_ids = get_all_slack_ids(client)
-    instructors_ids = get_instructors_ids(client)
+    instructors_ids = get_instructors_ids(client, INSTRUCTORS_CHANNEL_ID)
     submitted_slack_ids = get_submitted_slack_ids(SLU_ID, URL)
     not_submitted_slack_ids = all_slack_ids - submitted_slack_ids - instructors_ids
 
@@ -99,6 +97,7 @@ if __name__ == "__main__":
     n_messages_sent = 0
     for slack_id in not_submitted_slack_ids:
         send_message(slack_id=slack_id, slu_id=SLU_ID)
+        n_messages_sent += 1
 
     logging.info(
         f"{n_messages_sent} students have received a reminder message on SLU {SLU_ID}."
